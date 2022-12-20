fdf478a6619835172ed815d415da184334d3675b
yaros <yaroslavdisiak@gmail.com>
2022-12-21 01:25:54 +0200

    ex2done

--- main.cpp
+++ main.cpp
@@ -1,1 +1,20 @@
 
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main(){
+    const float Pi = 3.14;
+    double R,S,L;
+    cout << "Enter radius of circles"<< endl;
+    cout << "R: ";
+    cin >> R;
+    S = Pi * pow(R,2);
+    L = 2 * Pi * R;
+
+    cout << "S = " << S <<"\n"<< " L = " << L<<endl;
+
+
+
+    return 0;
+}
