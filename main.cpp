8240a6e403fbad2c12af1f803b9bea85eff3adad
yaros <yaroslavdisiak@gmail.com>
2022-12-21 01:13:25 +0200

    ex1done

--- main.cpp
+++ main.cpp
@@ -1,1 +1,19 @@
 
+
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+int main(){
+    float R0,R1,R2,R3;
+    cout<<"Enter 3 supports"<<endl;
+    cout <<" R1: ";
+    cin >> R1 ;
+    cout <<"R2: ";
+    cin >> R2;
+    cout <<"R3: ";
+    cin >> R3;
+    R0 = 1/R1 + 1/R2 + 1/R3;
+    cout <<"R0 = " << pow(R0,-1);
+    return  0;
+}
