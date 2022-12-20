fa32f54842364cacc2d1741b9474d927cfb38af2
yaros <yaroslavdisiak@gmail.com>
2022-12-21 01:32:36 +0200

    ex3done

--- main.cpp
+++ main.cpp
@@ -1,1 +1,19 @@
 
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main(){
+    double S,v,t,a;
+    cout<<"A program for calculating the distance traveled in rectilinear, uniformly accelerated motion"<<endl;
+    cout <<" Enter speed: ";
+    cin >> v;
+    cout<<"Enter time: ";
+    cin >>t;
+    cout <<"Enter acceleration: ";
+    cin >> a;
+    S = v*t+(a*pow(t,2))/2;
+    cout <<"Result"<<endl;
+    cout <<" S = "<< S;
+    return 0;
+}
